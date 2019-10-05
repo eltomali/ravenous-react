4d81bf39ed1c7d673cffd28771e01bd30f1ea4ca
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2019-10-05 20:07:38 +0200

    modify renderSortByOptions in SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -29,7 +29,7 @@ class SearchBar extends React.Component{
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map( sortByOption => {
 			const sortByOptionValue = this.sortByOptions[sortByOption];
-			return <li key={sortByOptionValue}>{sortByOption}</li>
+			return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
 		});
 	}
 	render() {
