ce16b6cd1664e2c7d09072e36f07273bf30ce071
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2019-10-07 15:22:22 +0200

    make Yelp.js with url to yelp and fetch promise

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,38 @@
 // Check Key
-if(process.env.REACT_APP_API_KEY === undefined){
+if(process.env.apiKey === undefined){
   console.error('No Api Key\nAdd `apiKey=<KEY>` in `.env` file');
   alert('No Api Key!\nAdd `apiKey=<KEY>` in `.env` file');
 }
 
+
+export const Yelp = {
+  search(term, location, sortBy) {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    
+    const httpOptions = {
+      headers: {Authorization: `Bearer ${process.env.apiKey}`}
+    }
+
+    return fetch(url, httpOptions)
+    .then( (response)=> {
+      return response.json;
+    })
+    .then( (jsonResponse) => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map( (business) => { return {
+          id: business.id, 
+          imageSrc: business.image_url,
+          name: business.name, 
+          address1: business.location.address1, 
+          address2: business.location.address2,
+          city: business.location.city, 
+          postnr: business.location.zip_code, 
+          category: business.categories, 
+          rating: business.rating, 
+          reviewCount: business.review_count
+        }
+        });
+      }
+    });
+  }
+}
